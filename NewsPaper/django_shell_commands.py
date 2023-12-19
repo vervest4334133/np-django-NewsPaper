e9d957528429f91be826031cf2db13f85420a3fb
Алекей <148230062+vervest4334133@users.noreply.github.com>
2023-12-19 20:02:32 +0300

    Initial commit

---
+++ NewsPaper/django_shell_commands.py
@@ -0,0 +1,62 @@
+cd NewsPaper
+
+python manage.py shell
+
+from news.models import *
+
+user1 = User.objects.create_user(username='First_user')
+user2 = User.objects.create_user(username='Second_user')
+
+Author.objects.create(author_user=user1)
+Author.objects.create(author_user=user2)
+
+Category.objects.create(category_name='StarWars')
+Category.objects.create(category_name='DeathMetal')
+Category.objects.create(category_name='AutoRepair')
+Category.objects.create(category_name='DragStar400')
+
+author = Author.objects.get(id=1) #author для проверки создания
+Post.objects.create(author_post=author, post_type='NE', post_name='Sequels in not in canon now!', post_content='123456')
+Post.objects.create(author_post=author, post_type='AR', post_name='article1', post_content='1111111')
+Post.objects.create(author_post=author, post_type='AR', post_name='article2', post_content='2222222')
+
+Post.objects.get(id=1).post_category.add(Category.objects.get(id=1))
+Post.objects.get(id=1).post_category.add(Category.objects.get(id=2))
+Post.objects.get(id=2).post_category.add(Category.objects.get(id=3))
+Post.objects.get(id=2).post_category.add(Category.objects.get(id=4))
+Post.objects.get(id=3).post_category.add(Category.objects.get(id=2))
+Post.objects.get(id=3).post_category.add(Category.objects.get(id=4))
+
+Comment.objects.create(post=Post.objects.get(id=1), user=Author.objects.get(id=1).author_user, comment_text='comment_1')    #post и user из связей таблиц в классе комментария
+Comment.objects.create(post=Post.objects.get(id=2), user=Author.objects.get(id=2).author_user, comment_text='comment_2')
+Comment.objects.create(post=Post.objects.get(id=3), user=Author.objects.get(id=1).author_user, comment_text='comment_3')
+Comment.objects.create(post=Post.objects.get(id=3), user=Author.objects.get(id=2).author_user, comment_text='comment_4')
+
+Comment.objects.get(id=1).like()
+Comment.objects.get(id=2).like()
+Comment.objects.get(id=2).like()
+Comment.objects.get(id=3).like()
+Comment.objects.get(id=4).like()
+
+Comment.objects.get(id=1).dislike()
+Comment.objects.get(id=1).dislike()
+Comment.objects.get(id=2).dislike()
+Comment.objects.get(id=4).dislike()
+
+a = Author.objects.get(id=1)
+a.update_rating()
+a._author_rating
+
+b = Author.objects.get(id=2)
+b.update_rating()
+b._author_rating
+
+best_user = Author.objects.order_by('-_author_rating').values('author_user', '_author_rating')[0]
+
+best_article = Post.objects.order_by('-rating').values('author_post', 'rating', 'post_name', 'time_of_creation')[0]
+
+best_article_preview = Post.objects.order_by('-rating')[0].preview()
+
+all_com = Post.objects.order_by('-rating')[0].comment_set.all().values('comment_date', 'user__username', 'rating', 'comment_text')
+
+# Post.objects.order_by('-rating')[0].comment_set.all()
---
+++ NewsPaper/news/models.py
@@ -0,0 +1,81 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Sum
+
+
+class Author(models.Model):
+    author_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    _author_rating = models.IntegerField(default=0, db_column='author_rating')
+
+    def update_rating(self):
+        # post_rating = self.post_set.aggregate(Sum('rating')).get('rating__sum') or 0
+        # comment_rating = self.author_user.comment_set.aggregate(Sum('rating')).get('rating__sum') or 0
+        # compost_rating = Comment.objects.filter(commentPost__postAuthor=self).aggregate(Sum('rating')).get('rating__sum') or 0
+        #
+        # self.rating = post_rating * 3 + comment_rating + compost_rating
+        # self.save()
+
+        pRating = self.post_set.aggregate(postRating=Sum('rating'))  #сбор данных поля, к которому применяется функция
+        pRat = 0
+        pRat += pRating.get('postRating')
+
+        cRating = self.author_user.comment_set.aggregate(commentRating=Sum('rating')) #замена цикла for
+        cRat = 0
+        cRat += cRating.get('commentRating')
+
+        self._author_rating = pRat * 3 + cRat
+        self.save()
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=64, unique=True)
+
+
+class Post(models.Model):
+    author_post = models.ForeignKey(Author, on_delete=models.CASCADE)
+    post_category = models.ManyToManyField(Category, through='PostCategory')
+
+    NEWS = 'NE'
+    ARTICLE = 'AR'
+    CATEGORY_CHOICES = (
+        (NEWS, 'Новость'),
+        (ARTICLE, 'Статья')
+    )
+
+    post_type = models.CharField(max_length=2, choices=CATEGORY_CHOICES, default=ARTICLE)
+    post_name = models.CharField(max_length=255)
+    post_content = models.TextField()
+    rating = models.IntegerField(default=0)
+    time_of_creation = models.DateTimeField(auto_now_add=True)
+
+    def preview(self):
+        return f'{self.post_content[0:123]}...'
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment_text = models.TextField()
+    comment_date = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
